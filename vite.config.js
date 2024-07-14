109ed50376499794b5aa7a3b344a1e2decfb33dc
AmandipSingh <singh.aman98140@gmail.com>
2024-07-14 17:58:34 +0530

    Added host and port in bite and json config files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: './', // Assuming index.html is in the root directory
+  server: {
+    host: '0.0.0.0',
+    port: 8080, // Ensure a known port number
+  },
   build: {
     outDir: 'dist', // Output directory for built files
     rollupOptions: {
